01e43c0e3751581c04314078978f3525cb33afca
Davin Hill <davin.hill@outlook.com>
2019-11-23 22:54:02 -0600

    conv cap module

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torchvision import models
-
+import torch.nn.functional as F
 
 
 # ======================================================
@@ -9,14 +9,14 @@ from torchvision import models
 # ======================================================
 vgg_pretrained = models.vgg16(pretrained = True, progress = False)
 class vgg_extraction(nn.Module):
-    def __init__(self):
+    def __init__(self, img_feat):
         super(vgg_extraction, self).__init__()
 
         self.feature_layers = vgg_pretrained.features  # all convolution layers in VGG, final layer is maxpool
         
         layers = []
         layers.append(vgg_pretrained.classifier[:-1])  # all fc layers, excluding final linear layer
-        layers.append(nn.Linear(4096, 512)) # apply fc layer. final output is 512 dim.
+        layers.append(nn.Linear(4096, img_feat)) # apply fc layer. final output is 512 dim.
         self.fc = nn.Sequential(*layers)
 
     def forward(self, x):
@@ -38,22 +38,59 @@ class attention(nn.Module):
         return x
         
         
-        
-        
+class ConvBlock(nn.Module):
+    def __init__(self, input_feat, output_feat, kernel_size, dropout_p):
+        super(ConvBlock, self).__init__()       
+
+        self.conv = nn.Conv1d(in_channels = input_feat, out_channels = output_feat, kernel_size = kernel_size, padding = kernel_size - 1, stride = 1)
+        self.dropout = nn.Dropout(p = dropout_p)
+        self.kernel_size = kernel_size
+        self.downsample = nn.Linear(input_feat, output_feat)
+
+    def forward(self, x):
+        import pdb;pdb.set_trace()
+        identity = x   # skip connection
+        x = self.conv(x) 
+        x = self.dropout(x)
+        x = F.glu(x, 1)  # reduces feature dimenion by 1/2. 
+        x = x[:,:,:-(self.kernel_size-1)]  # truncate by kernel_size - 1
+
+        # downsample the identity if there is a dimension mismatch (in the first conv layer)
+        if identity.shape != x.shape:
+            identity = identity.transpose(1, 2)
+            identity = self.downsample(identity)
+            identity = identity.transpose(1, 2)
+
+        return x + identity  
 
 # ======================================================
     # Convolution Captioning Model
 # ======================================================
 class conv_captioning(nn.Module):
-    def __init__(self, vocab_size):
+    def __init__(self, vocab_size, kernel_size, num_layers, dropout_p, word_feat, input_feat):
         super(conv_captioning, self).__init__()
 
-        self.word_embedding0 = nn.Embedding(vocab_size, 512)
-        self.word_embedding1 = nn.Linear(512, 512)
+        # Embedding Layers
+        self.word_embedding0 = nn.Embedding(vocab_size, word_feat)
+        self.word_embedding1 = nn.Linear(word_feat, word_feat)
+
+        # Convolution layers
+        conv_layers = []
+        for i in range(num_layers-1):
+            if i == 0:
+                output_feat = input_feat
+            else:
+                output_feat = input_feat * 2
+            conv_layers.append(ConvBlock(input_feat, kernel_size, dropout_p))
+        self.conv_n = nn.Sequential(*conv_layers)
+
+        # Classification layers
+        self.fc1 = nn.Linear(input_feat / 2, input_feat / 4)
+        self.fc2 = nn.Linear(input_feat / 4, vocab_size)
 
     def forward(self, caption_tknID, img_fc):
         
-        # Embedding Layer
+        # Embedding Layers
         word_embed = self.word_embedding0(caption_tknID)
         word_embed = self.word_embedding1(word_embed)
         # word_embed: n x (max_cap_len) x 512
@@ -61,10 +98,21 @@ class conv_captioning(nn.Module):
 
         # Reshape image embedding & concatenate with word embedding
         img_embed = img_fc.unsqueeze(1).expand(-1, word_embed.shape[1], -1)
-        input_embed = torch.cat((word_embed, img_embed), 2) # n x (max_cap_len) x 1024
+        input_embed = torch.cat((word_embed, img_embed), 2).transpose(1, 2) # n x 1024 x (max_cap_len)
+
+        # convolution layers
+        x = self.conv_n(input_embed) # n x 512 x max_cap_len
+
+        # classifier layers
+        x = x.transpose(1,2)  # n x max_cap_len x 512
+        x = self.fc1(x)
+        x = F.relu(x)  # n x max_cap_len x 256
+
+        x = self.fc2(x) # n x max_cap_len x vocab_size
+        x = x.transpose(1,2) # n x vocab_size x max_cap_len
 
 
-        return 0
+        return x
         
         
         
--- train.py
+++ train.py
@@ -18,12 +18,17 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--data_path', type=str, default=os.path.dirname(os.path.realpath(__file__)), help='path where data & annotations are located')
 parser.add_argument('--num_epochs', type=int, default=30)
-parser.add_argument('--vocab_size', type=int, default=10000)
+parser.add_argument('--vocab_size', type=int, default=9221)
 parser.add_argument('--max_cap_len', type=int, default=15, help = 'maximum caption length')
 parser.add_argument('--batch_size', type=int, default=128)
 parser.add_argument('--initial_lr', type=int, default=5 * np.exp(-5))
 parser.add_argument('--scheduler_gamma', type=int, default=0.1)
 parser.add_argument('--scheduler_stepsize', type=int, default=15)
+parser.add_argument('--num_layers', type=int, default=3, help = 'number of convolution layers')
+parser.add_argument('--kernel_size', type=int, default=5, help = 'size of 1d convolution kernel')
+parser.add_argument('--img_feat', type=int, default=512, help = 'number of features in image embedding layer. Should be divisible by 2.')
+parser.add_argument('--word_feat', type=int, default=512, help = 'number of features in word embedding layer. Should be divisible by 2.')
+parser.add_argument('--dropout_p', type=int, default=0.1, help = 'dropout probability parameter')
 
 args = parser.parse_args()
 
@@ -49,10 +54,10 @@ import pdb; pdb.set_trace()
 '''
 
 
-model_vgg = vgg_extraction()
+model_vgg = vgg_extraction(args.img_feat)
 model_vgg.to(device)
 
-model_cc = conv_captioning(args.vocab_size)
+model_cc = conv_captioning(args.vocab_size, args.kernel_size, args.num_layers, args.dropout_p, args.word_feat, args.img_feat + args.word_feat)
 model_cc.to(device)
 
 
