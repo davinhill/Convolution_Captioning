87a18e8cf2c441bbe558f75d05763703f313c750
Davin Hill <davin.hill@outlook.com>
2019-12-01 19:53:08 -0500

    encoding_rev2

--- coco-caption/pycocoevalcap/tokenizer/ptbtokenizer.py
+++ coco-caption/pycocoevalcap/tokenizer/ptbtokenizer.py
@@ -50,7 +50,7 @@ class PTBTokenizer:
         cmd.append(os.path.basename(tmp_file.name))
         p_tokenizer = subprocess.Popen(cmd, cwd=path_to_jar_dirname, \
                 stdout=subprocess.PIPE)
-        token_lines = p_tokenizer.communicate(input=sentences.rstrip())[0]
+        token_lines = p_tokenizer.communicate(input=sentences.rstrip())[0].encode()
         lines = token_lines.split('\n')
         # remove temp file
         os.remove(tmp_file.name)
