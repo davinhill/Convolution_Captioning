125d3a996bd5fcee8770852c52b5ec1e997a6c31
Blue Waters Trainee 178 <tra178@h2ologin2.ncsa.illinois.edu>
2019-12-07 15:59:26 -0600

    add to repo

---
+++ img_encoders.py
@@ -0,0 +1,94 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+import torch.nn.functional as F
+
+
+# ======================================================
+# VGG16 Model for extracting image features
+# ======================================================
+vgg_pretrained = models.vgg16(pretrained=True)
+
+
+class vgg_extraction(nn.Module):
+    def __init__(self, img_feat):
+        super(vgg_extraction, self).__init__()
+
+        # all convolution layers in VGG, final layer is maxpool
+        self.feature_layers = vgg_pretrained.features
+
+        layers = []
+        # all fc layers, excluding final linear layer
+        layers.append(vgg_pretrained.classifier[:-1])
+        # apply fc layer. final output is 512 dim.
+        layers.append(nn.Linear(4096, img_feat))
+        self.fc = nn.Sequential(*layers)
+
+    def forward(self, x):
+        # output of convolutions layers (for attention calculations)
+        output_conv = self.feature_layers(x)
+        # output of fc layers (for image embedding)
+        output_fc = self.fc(torch.flatten(output_conv, 1))
+
+        return output_conv, output_fc
+
+
+
+
+# ======================================================
+# Pretrained resnet18 model for image extraction
+# ======================================================
+resnet_pretrained = models.resnet18(pretrained=True)
+
+
+class resnet_extraction(nn.Module):
+    def __init__(self, img_feat):
+        super(resnet_extraction, self).__init__()
+
+        # convolution output, for use with attention
+        # 7 x 7 x 512
+        self.feature_layers = nn.Sequential(*(list(resnet_pretrained.children())[:-2]))
+        
+        self.pool = nn.AvgPool2d(kernel_size = 7, stride = 1, padding = 0)
+        self.fc = nn.Linear(512, 512)
+
+    def forward(self, x):
+        # output of convolutions layers (for attention calculations)
+        output_conv = self.feature_layers(x)
+
+        # output of fc layers (for image embedding)
+        output_fc = self.pool(output_conv)
+        output_fc = torch.flatten(output_fc, 1)
+        output_fc = self.fc(output_fc)
+
+        return output_conv, output_fc
+
+
+
+
+
+# ======================================================
+# Pretrained resnet18 model for image extraction
+# ======================================================
+densenet_pretrained = models.densenet121(pretrained=True)
+
+class densenet_extraction(nn.Module):
+    def __init__(self, img_feat):
+        super(densenet_extraction, self).__init__()
+
+        # 7 x 7 x 1024
+        self.feature_layers = densenet_pretrained.features
+        self.downsample = nn.Conv2d(in_channels = 1024, out_channels = 512, kernel_size = 1, stride = 1, padding = 0)
+        self.fc = nn.Linear(1024, 512)
+
+    def forward(self, x):
+        # output of convolutions layers (for attention calculations)
+        x = self.feature_layers(x)
+        output_conv = self.downsample(x)
+
+        # output of fc layers (for image embedding)
+        output_fc = F.adaptive_avg_pool2d(x, (1, 1))
+        output_fc = torch.flatten(output_fc, 1)
+        output_fc = self.fc(output_fc)
+
+        return output_conv, output_fc
