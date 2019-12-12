c78c7671b6712f33721216952f0c8a020c5477ec
Davin Hill <davin.hill@outlook.com>
2019-12-11 20:25:04 -0600

    but

--- train.py
+++ train.py
@@ -92,6 +92,7 @@ criterion = nn.CrossEntropyLoss()
 
 test_scores = [] # initialize saved scores
 init_epoch = 0
+id_to_word = np.load('./embed/id_to_word.npy')
 
 # ======================================================
     # Load Model
@@ -196,7 +197,7 @@ for epoch in range(init_epoch, args.num_epochs):
 
             
             # Print an example caption
-            z, _, _ = gen_caption(image, model_vgg, model_cc, args.vocab_size, args.max_cap_len)
+            z, _, _ = gen_caption(image, model_vgg, model_cc, args.vocab_size, id_to_word, args.max_cap_len)
             print('TEST------------------')
             print(z[0])
 
